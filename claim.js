4cf679bdd6c7c44b7adecfd52ea3ebeb546e5c4a
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-05 14:39:15 -0400

    make it better

--- claim.js
+++ claim.js
@@ -223,7 +223,7 @@ async function unknown_error() {
   await sleep(5000);
 }
 
-await cs1_claim_rplanet();
+// await cs1_claim_rplanet();
 // let claimed = await cs1_claim_rplanet();
 
 // console.log(" rpc  | " + rpc.endpoint);
