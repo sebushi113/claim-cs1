fdfdd08b6201e68428e66eefd2cb2478db66ee19
sebushi113 <41740143+sebushi113@users.noreply.github.com>
2023-06-15 19:46:52 -0400

    kuro

--- index.js
+++ index.js
@@ -1,93 +1,24 @@
-// const express = require("express");
-import express from "express";
-// const db = require("cyclic-dynamodb");
-import CyclicDb from "cyclic-dynamodb";
+import CyclicDB from "@cyclic.sh/dynamodb";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
-const db = CyclicDb(process.env.db);
+const db = CyclicDb(process.env.cyclicDB);
 
-const app = express();
+const set = async function () {
+  let cpu4 = db.collection("cpu4");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  let amounts = await cpu4.set("amounts", {
+    ccs3_cs1d: cpu4_cs1d,
+    ccs3_cd3d: cpu4_cd3d,
+    tw: tw,
+  });
+  console.log(amounts);
+};
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
+const get = async function () {
+  let cpu4 = db.collection("cpu4");
 
-// Create or Update an item
-app.post("/:col/:key", async (req, res) => {
-  console.log(req.body);
-
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).set(key, req.body);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-// Delete an item
-app.delete("/:col/:key", async (req, res) => {
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).delete(key);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-// Get a single item
-app.get("/:col/:key", async (req, res) => {
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} get key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).get(key);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-// Get a full listing
-app.get("/:col", async (req, res) => {
-  const col = req.params.col;
-  console.log(
-    `list collection: ${col} with params: ${JSON.stringify(req.params)}`
-  );
-  const items = await db.collection(col).list();
-  console.log(JSON.stringify(items, null, 2));
-  res.json(items).end();
-});
-
-// Catch all handler for all other request.
-app.use("*", (req, res) => {
-  res.json({ msg: "no route handler found" }).end();
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`index.js listening on ${port}`);
-});
+  let item = await cpu4.get("amounts");
+  console.log(item);
+};
